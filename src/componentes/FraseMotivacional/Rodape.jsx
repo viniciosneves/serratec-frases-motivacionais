31821c7f181003d1d723f803eb42c51a968a7ef1
Vinicios Neves <vinicios@gipsydanger.local>
2021-06-15 13:37:15 -0300

    adicionando rodap[e

---
+++ src/componentes/FraseMotivacional/Rodape.jsx
@@ -0,0 +1,23 @@
+function Rodape (props) {
+
+  const opcoes = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  }
+  const data = props.criadaEm.toLocaleString('pt-BR', opcoes)
+  // Fragment
+  return <>
+    <p>
+      {props.autor}
+    </p>
+    <p>
+      Postada em: <strong>{data}</strong>
+    </p>
+    <hr />
+  </>
+  
+}
+
+export default Rodape
