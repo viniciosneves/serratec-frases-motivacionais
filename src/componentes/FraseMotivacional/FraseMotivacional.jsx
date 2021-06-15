86619187f71e3ec7649f3eeae16bbebb44712e14
Vinicios Neves <vinicios@gipsydanger.local>
2021-06-15 10:50:30 -0300

    iniciando o repo

---
+++ src/componentes/FraseMotivacional/FraseMotivacional.jsx
@@ -0,0 +1,14 @@
+import './estilos.css'
+
+function FraseMotivacional () {
+  return <div>
+    <p className="frase">
+      São as nossas escolhas, mais do que as nossas capacidades, que mostram quem realmente somos
+    </p>
+    <p>
+      Alvo Dumbledore
+    </p>
+  </div>
+}
+
+export default FraseMotivacional
