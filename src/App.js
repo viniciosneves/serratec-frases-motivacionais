3f340bb7c73582cf535391fa087dc27f75f1d9a9
Vinicios Neves <vinicios@gipsydanger.local>
2021-06-15 11:59:23 -0300

    adicionando data

--- src/App.js
+++ src/App.js
@@ -6,24 +6,32 @@ function App() {
   const frases = [
     {
       texto: 'São as nossas escolhas, mais do que as nossas capacidades, que mostram quem realmente somos',
-      autor: 'Alvo Dumbledore'
+      autor: 'Alvo Dumbledore',
+      criadaEm: new Date(2021, 5, 1)
     },
     {
       texto: 'Vocês vão ter que me engulir',
-      autor: 'Técnico Zagalo'
+      autor: 'Técnico Zagalo',
+      criadaEm: new Date(2021, 5, 2)
     },
     {
       texto: '- Doi né? - O que? - deitar no sofá e lembrar que esqueceu o controle',
-      autor: 'Autor desconhecido'
+      autor: 'Autor desconhecido',
+      criadaEm: new Date(2021, 5, 3)
     },
   ]
 
   return (
     <div>
       <h1>Frases motivacionais para alegrar o seu dia!!</h1>
-      <FraseMotivacional frase={frases[0].texto} autor={frases[0].autor} />
-      <FraseMotivacional frase={frases[1].texto} autor={frases[1].autor} />
-      <FraseMotivacional frase={frases[2].texto} autor={frases[2].autor} />
+      
+      <FraseMotivacional 
+        frase={frases[0].texto} 
+        autor={frases[0].autor} 
+        criadaEm={frases[0].criadaEm}/>
+
+      <FraseMotivacional frase={frases[1].texto} autor={frases[1].autor} criadaEm={frases[1].criadaEm}/>
+      <FraseMotivacional frase={frases[2].texto} autor={frases[2].autor} criadaEm={frases[2].criadaEm}/>
     </div>
   );
 }
--- src/componentes/FraseMotivacional/FraseMotivacional.jsx
+++ src/componentes/FraseMotivacional/FraseMotivacional.jsx
@@ -1,12 +1,24 @@
 import './estilos.css'
 
 function FraseMotivacional (props)  {
+
+  const opcoes = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  }
+  const data = props.criadaEm.toLocaleString('pt-BR', opcoes)
+
   return <div>
     <p className="frase">
-      {props.frase}
+      - "{props.frase}"
     </p>
     <p>
       {props.autor}
+    </p>
+    <p>
+      Postada em: <strong>{data}</strong>
     </p>
   </div>
 }
